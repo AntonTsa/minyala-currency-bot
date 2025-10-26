ac3cef91102b4dba5a72afce8a2120a6095822d4
Anton Tsarenko <58445366+AntonTsa@users.noreply.github.com>
2025-10-26 22:22:45 +0200

    Delete unused UserSettings class
    Removed unused UserSettings class and its properties.

--- src/main/java/org/project/ninjas/minyala/currency/bot/settings/UserSettings.java
+++ src/main/java/org/project/ninjas/minyala/currency/bot/settings/UserSettings.java
@@ -25,25 +25,3 @@ public class UserSettings {
         this.userId = userId;
     }
 }
-
-
-
-
-
-
-
-
-
-@Data
-public class UserSettings {
-    private Long chatId;
-    private int decimalPlaces = 2;
-    private String bank = "Приватбанк";
-    private List<String> currency = List.of("USD");
-    private String notifyTime = "09:00"; 
-    private boolean notificationsEnabled = true;
-
-    public UserSettings(Long chatId) {
-        this.chatId = chatId;
-    }
-}
